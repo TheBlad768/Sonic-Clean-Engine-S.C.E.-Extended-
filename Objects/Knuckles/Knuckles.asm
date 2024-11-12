196142849cd5214c7b2fe20895814e7fd967f70a
TheBlad768 <master.metalsonic2014@gmail.com>
2024-11-12 06:51:32 +0400

    bsr => jsr

--- Objects/Knuckles/Knuckles.asm
+++ Objects/Knuckles/Knuckles.asm
@@ -879,7 +879,7 @@ Knuckles_Wall_Climb:
 		moveq	#9,d2
 		add.w	y_pos(a0),d2
 		move.w	x_pos(a0),d3
-		bsr.w	sub_F828
+		jsr	(sub_F828).w
 
 		; check if Knuckles has room below him
 		tst.w	d1
@@ -926,7 +926,7 @@ Knuckles_Wall_Climb:
 		move.w	y_pos(a0),d2
 		addq.w	#8,d2
 		move.w	x_pos(a0),d3
-		bsr.w	sub_F828
+		jsr	(sub_F828).w
 
 		; Check if Knuckles has room above him.
 		tst.w	d1
@@ -982,7 +982,7 @@ Knuckles_Wall_Climb:
 		moveq	#9,d2
 		add.w	y_pos(a0),d2
 		move.w	x_pos(a0),d3
-		bsr.w	sub_F828
+		jsr	(sub_F828).w
 
 		; Check if Knuckles has room below him.
 		tst.w	d1
